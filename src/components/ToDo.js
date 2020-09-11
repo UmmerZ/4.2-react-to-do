627f0707329c8df566bebe62209a3bd3b92d2308
ummerZ <ummer.zaman88@gmail.com>
2020-09-11 11:53:48 -0600

    populated ul

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
+ 
 
-export default function ToDo()
-{
+
+
+export default function ToDo() {
+    const [newTask, setNewTask] = useState();
+// in  useState() is the default value for this state.
+    const [toDos, setToDos] = useState([
+        {
+            task: "Buy Milk"
+        },
+        {
+            task: "Learn React"
+        },
+        {
+            task: "Find out what React is"
+        }
+    ].map((toDo,index) => <li key={index}>{toDo.task}</li>))
     return (
         <>
             <form>
                 <label htmlFor="task">New Task:</label>
-                <input type="text" id="task" />
-                <input type="submit" value="Add To-Do"/>
+                <input
+                    type="text"
+                    id="task"
+                    onChange={e => { setNewTask(e.target.value) }}
+                    value={newTask} />
+             
+                <input type="submit" value="Add To-Do" />
+                <p>
+                    <strong>Task:</strong>
+                        <input value={newTask}/>
+                    
+                </p>
             </form>
             <ul>
-                
+              {toDos}
             </ul>
         </>
     );
